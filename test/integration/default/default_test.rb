1db56d983ddcfcbb2e9a9f885f3653dd6e9d1896
HackerShark <melsharkawi@mitre.org>
2018-05-11 12:54:00 -0400

    cleaning up test for default
    Signed-off-by: HackerShark <melsharkawi@mitre.org>

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -17,9 +17,9 @@ describe port(80), :skip do
   it { should_not be_listening }
 end
 
-describe command('curl http://localhost:8080') do
-  its(:stdout) { should match /Tomcat/ }
-end
+# describe command('curl http://localhost:8080') do
+#   its(:stdout) { should match /Tomcat/ }
+# end
 
 describe package('java-1.7.0-openjdk-devel') do
   it { should be_installed }
